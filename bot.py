3587a009a08189759d429d59cfa54ef0e4f38074
John Carneiro <john@sizy.com.br>
2019-10-24 09:43:10 -0300

    Responde os endereços

--- bot.py
+++ bot.py
@@ -24,33 +24,31 @@ def local_botoes(message):
     bot.send_message(message.chat.id, "Para onde você quer ir?", reply_markup=keyboard)
 
 
-@bot.callback_query_handler(func=lambda callback: callback.data == "local_palestras")
-def local_palestras(callback):
-    bot.edit_message_text((
+@bot.callback_query_handler(func=lambda callback: callback.data in ["local_palestras", "local_tutoriais_sprints"])
+def local_enderecos(callback):
+    if callback.data == "local_palestras":
+        message = (
             "As palestras da *Python Brasil 2019* acontecerão no endereço:\n"
             "Centro de Convenções Ribeirão Preto\n"
             "R. Bernardinho de Campos, 999 - Centro"
-        ),
+        )
+        latitude = -21.1748969
+        longitude = -47.8098745
+    else:
+        message = (
+            "Os tutoriais e sprints da *Python Brasil 2019* acontecerão no endereço:\n"
+            "Estácio Centro Universitário\n"
+            "Rua Abrahão Issa Halach, 980 - Ribeirânia, Ribeirão Preto - SP, 14096-160"
+        )
+        latitude = -21.1929639
+        longitude =  -47.8174115
+
+    bot.edit_message_text(message,
         callback.message.chat.id,
         callback.message.message_id,
         parse_mode="markdown"
     )
-    bot.send_location(callback.message.chat.id, -21.1748969, -47.8098745)
-
-
-@bot.callback_query_handler(func=lambda callback: callback.data == "local_tutoriais_sprints")
-def local_tutoriais_sprints(callback):
-    pass
-
-
-# @bot.message_handler(commands=["local", "endereco"])
-# def local(message):
-#     bot.send_message(message.chat.id, (
-#         "As palestras da *Python Brasil 2019* acontecerão no endereço:\n"
-#         "Centro de Convenções Ribeirão Preto\n"
-#         "R. Bernardinho de Campos, 999 - Centro"
-#     ), parse_mode="markdown")
-#     bot.send_location(message.chat.id, -21.1748969, -47.8098745)
+    bot.send_location(callback.message.chat.id, latitude, longitude)
 
 
 bot.polling()
